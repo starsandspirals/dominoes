be58ab0a9cb35ca84aa9927d2064f0e5fd863a73
Daniel Marshall <dmarshall2@sheffield.ac.uk>
2017-12-13 12:16:28 +0000

    Made code neater, added score 59 strategy

--- dominoplayers.hs
+++ dominoplayers.hs
@@ -1,97 +1,102 @@
 import DomsMatch
 
-type Plays = ([Dom], [Dom])
+type Situation = GameState -> Bool
 
-type Situation = Plays -> Player -> Scores -> DomBoard -> Bool
-
-type Strategy = Plays -> DomsPlayer
+type Strategy = GameState -> Move
 
 type Tactic = (Situation, Strategy)
 
 hsdSituation :: Situation
-hsdSituation _ _ _ _ = True
+hsdSituation _ = True
 
 hsdStrategy :: Strategy
-hsdStrategy _ h b p s = hsdPlayer h b p s
+hsdStrategy (h,_,p,b,s) = hsdPlayer h b p s
 
 hsdTactic :: Tactic
 hsdTactic = (hsdSituation, hsdStrategy)
 
 firstDropSituation :: Situation
-firstDropSituation (h,_) _ _ InitBoard = elem (5,4) h
-firstDropSituation _ _ _ board = False
+firstDropSituation (h,_,_,InitBoard,_) = elem (5,4) h
+firstDropSituation (_,_,_,b,_) = False
 
 firstDropStrategy :: Strategy
-firstDropStrategy _ _ _ _ _ = ((5,4),L)
+firstDropStrategy _ = ((5,4),L)
 
 firstDropTactic :: Tactic
 firstDropTactic = (firstDropSituation, firstDropStrategy)
 
 score61Situation :: Situation
-score61Situation (h,_) p (s1, s2) board
-  | p == P1 && s1 > 53 = True
-  | p == P2 && s2 > 53 = True
-  | otherwise = False
+score61Situation (h,_,p,b,s) = (playerScore p s) > 53
 
 score61Strategy :: Strategy
-score61Strategy _ h b p (s1, s2) = move
+score61Strategy gs = scoreStrategy 61 gs
+
+scoreStrategy :: Int -> Strategy
+scoreStrategy i gs@(h,_,p,b,s) = move
   where
-    score
-      | p == P1 = s1
-      | otherwise = s2
-    difference = 61 - score
+    score = playerScore p s
+    difference = i - score
     scoringmoves = scoreN b difference
     moves = filter (\(x,_) -> elem x h) scoringmoves
+    first:_ = moves
     move
-      | null moves = hsdPlayer h b p (s1,s2)
-      | otherwise = head moves
+      | null moves && i == 61 = scoreStrategy 59 gs
+      | null moves = hsdPlayer h b p s
+      | otherwise = first
 
 score61Tactic :: Tactic
 score61Tactic = (score61Situation, score61Strategy)
     
-playerFramework :: Hand -> DomBoard -> Player -> Scores -> [Tactic] -> Move
+playerFramework :: GameState -> [Tactic] -> Move
 
-playerFramework h InitBoard p s tactics = move
+playerFramework (h1,h2,p,b,s) tactics = move
   where
-    remainingDoms = filter (\x -> not (elem x h)) domSet
-    plays = (h, remainingDoms)
-    tactic:_ = filter (\(situation, _) -> situation plays p s InitBoard) tactics
+    state = (h1, h2, p, b, s)
+    tactic:_ = filter (\(situation, _) -> situation state) tactics
     (_, strategy) = tactic
-    move = strategy plays h InitBoard p s
+    move = strategy state
 
-playerFramework h board p s tactics = move
+opponentPlays :: Hand -> DomBoard -> Hand
+
+opponentPlays h InitBoard = plays
   where
-    remainingDoms = filter (\x -> not (elem x h)) domSet
-    (Board _ _ history) = board
-    playedDoms = map (\(x, _, _) -> x) history
-    unplayedDoms = filter (\x -> not (elem x playedDoms)) remainingDoms
-    plays = (h, unplayedDoms)
-    tactic:_ = filter (\(situation, _) -> situation plays p s board) tactics
-    (_, strategy) = tactic
-    move = strategy plays h board p s
+    plays = filter (\x -> not (elem x h)) domSet
+    
+opponentPlays h b = plays
+  where
+    remaining = filter (\x -> not (elem x h)) domSet
+    (Board _ _ history) = b
+    used = map (\(x,_,_) -> x) history
+    plays = filter (\x -> not (elem x used)) remaining
+
+playerScore :: Player -> Scores -> Int
+playerScore P1 (s,_) = s
+playerScore P2 (_,s) = s
 
 easyPlayer :: DomsPlayer
-easyPlayer h db p s = move
+easyPlayer h1 db p s = move
   where
     tactics = [hsdTactic]
-    move = playerFramework h db p s tactics
+    h2 = opponentPlays h1 db
+    move = playerFramework (h1,h2,p,db,s) tactics
 
 mediumPlayer :: DomsPlayer
-mediumPlayer h db p s = move
+mediumPlayer h1 db p s = move
   where
     tactics = [firstDropTactic, score61Tactic, hsdTactic]
-    move = playerFramework h db p s tactics
+    h2 = opponentPlays h1 db
+    move = playerFramework (h1,h2,p,db,s) tactics
 
-scoreN :: DomBoard -> Int -> [ (Dom, End) ]
+scoreN :: DomBoard -> Int -> [(Dom, End)]
 
 scoreN b i = scoreNA b i domSet
 
-scoreNA :: DomBoard -> Int -> [Dom] -> [ (Dom, End) ]
+scoreNA :: DomBoard -> Int -> [Dom] -> [(Dom, End)]
  
 scoreNA b i [d]
-  | goesLP d b && goesRP d b && sL == i && sR == i = [ (d, L), (d, R) ]
-  | goesLP d b && sL == i = [ (d, L) ]
-  | goesRP d b && sR == i = [ (d, R) ]
+  | goesLP d b && goesRP d b && sL == i && sR == i = [(d,L),(d,R)]
+  | goesLP d b && sL == i = [(d,L)]
+  | goesRP d b && sR == i = [(d,R)]
   | otherwise = []
   where sL = scoreboard (resMaybe (playDom P1 d L b))
         sR = scoreboard (resMaybe (playDom P1 d R b))
